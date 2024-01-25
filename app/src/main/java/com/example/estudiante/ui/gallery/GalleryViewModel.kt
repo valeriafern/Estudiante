e16ce05cb1296eaf3f5e86635cbca75f9c656975
valeriafern <vfernandez321@misena.edu.co>
2024-01-24 22:30:59 -0500

    listo menu

--- app/src/main/java/com/example/estudiante/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/estudiante/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Solicitar Clase"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/estudiante/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/estudiante/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Perfil"
     }
     val text: LiveData<String> = _text
 }
